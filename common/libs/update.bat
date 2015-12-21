272eef9a03172d3ecd2275bbc4e222b660393d90
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2015-12-21 16:10:28 +0300

    [win-nix] скрипт подкачки библиотек. добавлены библиотеки
    Former-commit-id: 263877fdecf96ebcee642e6db1e61044c9634815

--- common/libs/update.bat
+++ common/libs/update.bat
@@ -1,9 +1,31 @@
 @echo off
-rem set LibsPath=svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents/DesktopEditor/ChromiumBasedEditors2
 set LibsPath=svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk/ServerComponents/DesktopEditor
 
 if exist ".svn" (
-    TortoiseProc.exe /command:update /path:.\ /closeonend:0
+    svn update
 ) else (
-    TortoiseProc.exe /command:checkout /blockpathadjustments /path:.\ /url:%LibsPath% /closeonend:0
+    svn checkout %LibsPath% ./. --depth empty
+    svn update --set-depth infinity ChromiumBasedEditors2
+    svn update --set-depth infinity common
+    svn update --set-depth infinity Word_Api
 )
+
+set trunk_path=svn://fileserver/activex/AVS/Sources/TeamlabOffice/trunk
+set export_path=..\converter\windows
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+if %OS%==32BIT (
+    set os_path=win32
+    svn export --force %trunk_path%/AsyncServerComponents/Bin/Windows/x2t32.exe %export_path%/%os_path%
+    svn export --force %trunk_path%/ServerComponents/SDK/lib/win_32 %export_path%/%os_path%
+) else (
+    set os_path=win64
+    svn export --force %trunk_path%/AsyncServerComponents/Bin/Windows/x2t.exe %export_path%/%os_path%
+    svn export --force %trunk_path%/ServerComponents/SDK/lib/win_64/ %export_path%/%os_path%
+)
+
+svn export --force %trunk_path%/ServerComponents/SDK/bin/windows/icudt.dll %export_path%
+svn export --force %trunk_path%/ServerComponents/UnicodeConverter/icubuilds/%os_path%/bin/icudt55.dll %export_path%/%os_path%
+svn export --force %trunk_path%/ServerComponents/UnicodeConverter/icubuilds/%os_path%/bin/icuuc55.dll %export_path%/%os_path%
+
+rd /s /q %export_path%\%os_path%\debug
+del %export_path%\%os_path%\*.lib %export_path%\%os_path%\html*.exe
