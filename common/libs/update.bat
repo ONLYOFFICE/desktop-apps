193f9fe4535836ff4ea43c448e5caf75b64c75b8
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2015-12-24 11:13:59 +0300

    [win-nix] отладка скрипта обновления библиотек
    Former-commit-id: 6b27059deee1b0c084609c78eb43b57774ad3cfd

--- common/libs/update.bat
+++ common/libs/update.bat
@@ -10,28 +10,26 @@ if exist ".svn" (
     svn update --set-depth infinity Word_Api
 )
 
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+%OS%==32BIT && set OSNAME=win32 || set OSNAME=win64
+
 set trunk_path=svn://192.168.3.15/activex/AVS/Sources/TeamlabOffice/trunk
 set export_path=..\converter\windows
-reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
 if %OS%==32BIT (
-    set os_path=win32
+    md %export_path%\%OSNAME%
 
-    md %export_path%\%os_path%
-
-    svn export --force %trunk_path%/AsyncServerComponents/Bin/Windows/x2t32.exe %export_path%/%os_path%
-    svn export --force %trunk_path%/ServerComponents/SDK/lib/win_32 %export_path%/%os_path%
+    svn export --force %trunk_path%/AsyncServerComponents/Bin/Windows/x2t32.exe %export_path%\%OSNAME%
+    svn export --force %trunk_path%/ServerComponents/SDK/lib/win_32 %export_path%\%OSNAME%
 ) else (
-    set os_path=win64
+    md %export_path%\%OSNAME%
 
-    md %export_path%\%os_path%
-
-    svn export --force %trunk_path%/AsyncServerComponents/Bin/Windows/x2t.exe %export_path%/%os_path%
-    svn export --force %trunk_path%/ServerComponents/SDK/lib/win_64/ %export_path%/%os_path%
+    svn export --force %trunk_path%/AsyncServerComponents/Bin/Windows/x2t.exe %export_path%\%OSNAME%
+    svn export --force %trunk_path%/ServerComponents/SDK/lib/win_64/ %export_path%\%OSNAME%
 )
 
 svn export --force %trunk_path%/ServerComponents/SDK/bin/windows/icudt.dll %export_path%
-svn export --force %trunk_path%/ServerComponents/UnicodeConverter/icubuilds/%os_path%/bin/icudt55.dll %export_path%/%os_path%
-svn export --force %trunk_path%/ServerComponents/UnicodeConverter/icubuilds/%os_path%/bin/icuuc55.dll %export_path%/%os_path%
+svn export --force %trunk_path%/ServerComponents/UnicodeConverter/icubuilds/%OSNAME%/bin/icudt55.dll %export_path%\%OSNAME%
+svn export --force %trunk_path%/ServerComponents/UnicodeConverter/icubuilds/%OSNAME%/bin/icuuc55.dll %export_path%\%OSNAME%
 
-rd /s /q %export_path%\%os_path%\debug
-del %export_path%\%os_path%\*.lib %export_path%\%os_path%\html*.exe
+rd /s /q %export_path%\%OSNAME%\debug
+del %export_path%\%OSNAME%\*.lib %export_path%\%OSNAME%\html*.exe
