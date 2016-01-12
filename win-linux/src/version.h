0eca8835c48fb9c7c326ddfb4e35e092de993ed1
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2016-01-12 15:33:04 +0300

    [win-nix] файлы открывались до проверки лицензии
    Former-commit-id: 5fc2d3362e31163715276060c5055be3579a526c

--- win-linux/src/version.h
+++ win-linux/src/version.h
@@ -33,8 +33,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             0,2,2,152
-#define VER_FILEVERSION_STR         "0.2.2.152\0"
+#define VER_FILEVERSION             0,2,2,160
+#define VER_FILEVERSION_STR         "0.2.2.160\0"
 
 #define VER_PRODUCTVERSION          VER_FILEVERSION
 #define VER_PRODUCTVERSION_STR      "0.2\0"
