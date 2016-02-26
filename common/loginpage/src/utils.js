cd535c98b37af7dceee4d32ec19e4f662138060a
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2016-02-26 17:47:38 +0300

    [start page] отладка страницы about
    Former-commit-id: 75d190d06755a7478637d852f3670198ff090cd6

--- common/loginpage/src/utils.js
+++ common/loginpage/src/utils.js
@@ -200,11 +200,29 @@ utils.fn.extend = function(dest, src) {
 };
 
 utils.fn.parseVersion = function(str) {
-    var _re_edition = /edition\:([\w\s]+)/.exec(str),
-        _re_version = /num\:([\d\.]+)/.exec(str);
+    var _re_edition = /edition\:([^\;?]+)/.exec(str),
+        _re_version = /num\:([\d\.]+)/.exec(str),
+        _re_active = /active\:([^\;?]+)/.exec(str);
 
     return {
         edition: !!_re_edition ? _re_edition[1] : undefined,
-        version: !!_re_version ? _re_version[1] : undefined
+        version: !!_re_version ? _re_version[1] : undefined,
+        active: !!_re_active ? _re_active[1] : undefined
     }
 };
+
+function getUrlParams() {
+    var e,
+    a = /\+/g,  
+    r = /([^&=]+)=?([^&]*)/g,
+    d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
+    q = window.location.search.substring(1),
+    urlParams = {};
+
+    while (e = r.exec(q))
+        urlParams[d(e[1])] = d(e[2]);
+    
+    return urlParams;
+}
+
+utils.inParams = getUrlParams();
