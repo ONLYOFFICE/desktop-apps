de4e4ef12327e0be6a8e2bb24b2c45329d524b15
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2016-03-04 15:50:14 +0300

    [win-nix] на XP x86 не подцеплялись иконки документов
    Former-commit-id: 7c56e76ae5b2d3d1bd7efe8e7832d69b381fda6e

--- win-linux/src/version.h
+++ win-linux/src/version.h
@@ -33,8 +33,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             1,1,2,188
-#define VER_FILEVERSION_STR         "1.1.2.188\0"
+#define VER_FILEVERSION             1,1,2,190
+#define VER_FILEVERSION_STR         "1.1.2.190\0"
 
 #define VER_PRODUCTVERSION          VER_FILEVERSION
 #define VER_PRODUCTVERSION_STR      "1.1\0"
