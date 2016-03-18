58cc2d93e0f2429d1d6fc043346f9508615f124a
Maxim Kadushkin <Maxim.Kadushkin@avsmedia.net>
2016-03-18 14:56:21 +0300

    [win-nix] deploy version
    Former-commit-id: 1540ad302d656723cf8527ba84c129b31647604d

--- win-linux/src/version.h
+++ win-linux/src/version.h
@@ -33,8 +33,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             1,1,2,198
-#define VER_FILEVERSION_STR         "1.1.2.198\0"
+#define VER_FILEVERSION             1,1,2,200
+#define VER_FILEVERSION_STR         "1.1.2.200\0"
 
 #define VER_PRODUCTVERSION          VER_FILEVERSION
 #define VER_PRODUCTVERSION_STR      "1.1\0"
