506f30260190ad86f5204a61cc2a3d45f44f4aff
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2016-06-27 15:15:21 +0300

    [win-nix] lang tools removed

--- win-linux/langs/lang_create.bat
+++
@@ -1,2 +0,0 @@
-C:\Qt\Qt5.5.1\5.5\msvc2013_64\bin\lupdate.exe ../ASCDocumentEditor.pro
-rem C:\Qt\Qt5.4.1\5.4\msvc2013_64_opengl\bin\linguist.exe lang.ts
--- win-linux/langs/lang_release.bat
+++
@@ -1,1 +0,0 @@
-C:\Qt\Qt5.6.0\5.6\msvc2013_64\bin\lrelease.exe ../ASCDocumentEditor.pro
