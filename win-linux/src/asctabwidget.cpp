039885a571c414ba0efa47745d42e5828a559c87
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2016-11-25 18:06:40 +0300

    [win-nix] removed wrong info from portal arguments

--- win-linux/src/asctabwidget.cpp
+++ win-linux/src/asctabwidget.cpp
@@ -250,11 +250,14 @@ int CAscTabWidget::addPortal(QString url, QString name)
 
     setProperty("empty", false);
 
+    if ( !url.contains(QRegularExpression("desktop=true")) )
+        url.append("/products/files/?desktop=true");
+
     QCefView* pView = new QCefView(this);
     pView->SetBackgroundCefColor(244, 244, 244);
     pView->setGeometry(0,0, size().width(), size().height() - tabBar()->height());
     pView->Create(m_pManager, cvwtSimple);
-    pView->GetCefView()->load((url + "/products/files/?desktop=true").toStdWString());
+    pView->GetCefView()->load(url.toStdWString());
     int id_view = pView->GetCefView()->GetId();
 
     QString portal = name.isEmpty() ? Utils::getPortalName(url) : name;
