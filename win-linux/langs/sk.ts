044f9ec2ae5b0d2770d35d57a3a11cf92e1b71eb
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2017-09-21 17:44:22 +0300

    [win-nix] added czech and slovak translations

---
+++ win-linux/langs/sk.ts
@@ -0,0 +1,271 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sk_SK">
+<context>
+    <name>CAscTabWidget</name>
+    <message>
+        <location filename="../src/asctabwidget.cpp" line="486"/>
+        <source>Document</source>
+        <translation>Dokument</translation>
+    </message>
+</context>
+<context>
+    <name>CDownloadWidget</name>
+    <message>
+        <location filename="../src/cdownloadwidget.cpp" line="122"/>
+        <source>Cancel</source>
+        <translation>Zrušiť</translation>
+    </message>
+</context>
+<context>
+    <name>CFileDialogWrapper</name>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="51"/>
+        <source>All files (*.*)</source>
+        <translation>Všetky súbory (*.*)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="53"/>
+        <source>DOCX Document (*.docx)</source>
+        <translation>DOCX Dokument (*.docx)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="54"/>
+        <source>DOC Document (*.doc)</source>
+        <translation>DOC Dokument (*.doc)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="55"/>
+        <source>ODT Document (*.odt)</source>
+        <translation>ODT Dokument (*.odt)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="56"/>
+        <source>RTF File (*.rtf)</source>
+        <translation>RTF Súbor (*.rtf)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="57"/>
+        <source>TXT File (*.txt)</source>
+        <translation>TXT Súbor (*.txt)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="58"/>
+        <source>HTML File (*.html)</source>
+        <translation>HTML Súbor (*.html)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="59"/>
+        <source>MHT File (*.mht)</source>
+        <translation>MHT Súbor (*.mht)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="60"/>
+        <source>EPUB File (*.epub)</source>
+        <translation>EPUB Súbor (*.epub)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="62"/>
+        <source>PPTX File (*.pptx)</source>
+        <translation>PPTX Súbor (*.pptx)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="63"/>
+        <source>PPT File (*.ppt)</source>
+        <translation>PPT Súbor (*.ppt)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="64"/>
+        <source>ODP File (*.odp)</source>
+        <translation>ODP Súbor (*.odp)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="65"/>
+        <source>PPSX File (*.ppsx)</source>
+        <translation>PPSX Súbor (*.ppsx)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="67"/>
+        <source>XLSX File (*.xlsx)</source>
+        <translation>XLSX Súbor (*.xlsx)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="68"/>
+        <source>XLS File (*.xls)</source>
+        <translation>XLS Súbor (*.xls)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="69"/>
+        <source>ODS File (*.ods)</source>
+        <translation>ODS Súbor (*.ods)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="70"/>
+        <source>CSV File (*.csv)</source>
+        <translation>CSV Súbor (*.csv)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="72"/>
+        <source>PDF File (*.pdf)</source>
+        <translation>PDF Súbor (*.pdf)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="73"/>
+        <source>DJVU File (*.djvu)</source>
+        <translation>DJVU Súbor (*.djvu)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="74"/>
+        <source>XPS File (*.xps)</source>
+        <translation>XPS Súbor (*.xps)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="116"/>
+        <location filename="../src/cfiledialog.cpp" line="122"/>
+        <source>Save As</source>
+        <translation>Uložiť ako</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="127"/>
+        <source>Yes</source>
+        <translation>Áno</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="127"/>
+        <source>No</source>
+        <translation>Nie</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="143"/>
+        <source>%1 already exists.&lt;br&gt;Do you want to replace it?</source>
+        <translation>%1 už existuje.&lt;br&gt;Chcete ho nahradiť?</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="166"/>
+        <source>Word Document</source>
+        <translation>Word Dokument</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="169"/>
+        <source>Excel Workbook</source>
+        <translation>Excel Zošit</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="172"/>
+        <source>PowerPoint Presentation</source>
+        <translation>PowerPoint Prezentácia</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="175"/>
+        <source>%1 File (*.%2)</source>
+        <translation>%1 Súbor (*.%2)</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="195"/>
+        <source>Open Document</source>
+        <translation>Otvoriť dokument</translation>
+    </message>
+    <message>
+        <location filename="../src/cfiledialog.cpp" line="201"/>
+        <source>Jpeg (*.jpeg *.jpg);;Png (*.png);;Gif (*.gif);;Bmp (*.bmp);;</source>
+        <translation>Jpeg (*.jpeg *.jpg);;Png (*.png);;Gif (*.gif);;Bmp (*.bmp);;</translation>
+    </message>
+</context>
+<context>
+    <name>CMainPanel</name>
+    <message>
+        <location filename="../src/cmainpanel.cpp" line="150"/>
+        <source>FILE</source>
+        <translation>SÚBOR</translation>
+    </message>
+    <message>
+        <location filename="../src/cmainpanel.cpp" line="1305"/>
+        <source>%1 is modified.&lt;br&gt;Do you want to keep changes?</source>
+        <translation>%1 bol upravený.&lt;br&gt;Chcete zmeny zachovať?</translation>
+    </message>
+    <message>
+        <location filename="../src/cmainpanel.cpp" line="505"/>
+        <location filename="../src/cmainpanel.cpp" line="697"/>
+        <source>Yes</source>
+        <translation>Áno</translation>
+    </message>
+    <message>
+        <location filename="../src/cmainpanel.cpp" line="505"/>
+        <location filename="../src/cmainpanel.cpp" line="697"/>
+        <source>No</source>
+        <translation>Nie</translation>
+    </message>
+    <message>
+        <location filename="../src/cmainpanel.cpp" line="505"/>
+        <source>Cancel</source>
+        <translation>Zrušiť</translation>
+    </message>
+    <message>
+        <location filename="../src/cmainpanel.cpp" line="673"/>
+        <location filename="../src/cmainpanel.cpp" line="719"/>
+        <source>File format not supported.</source>
+        <translation>Formát súboru nie je podporovaný.</translation>
+    </message>
+    <message>
+        <location filename="../src/cmainpanel.cpp" line="699"/>
+        <source>%1 doesn&apos;t exists!&lt;br&gt;Remove file from the list?</source>
+        <translation>%1 neexistuje.&lt;br&gt;Odstrániť súbor zo zoznamu?</translation>
+    </message>
+    <message>
+        <location filename="../src/cmainpanel.cpp" line="1218"/>
+        <source>Sign Up</source>
+        <translation>Zaregistrovať</translation>
+    </message>
+    <message>
+        <location filename="../src/cmainpanel.cpp" line="732"/>
+        <source>Document%1.docx</source>
+        <translation>Dokument%1.docx</translation>
+    </message>
+    <message>
+        <location filename="../src/cmainpanel.cpp" line="733"/>
+        <source>Book%1.xlsx</source>
+        <translation>Zošit%1.xlsx</translation>
+    </message>
+    <message>
+        <location filename="../src/cmainpanel.cpp" line="734"/>
+        <source>Presentation%1.pptx</source>
+        <translation>Prezentácia%1.pptx</translation>
+    </message>
+    <message>
+        <location filename="../src/cmainpanel.cpp" line="949"/>
+        <source>Print Document</source>
+        <translation>Vytlačiť dokument</translation>
+    </message>
+</context>
+<context>
+    <name>CPrintProgress</name>
+    <message>
+        <location filename="../src/cprintprogress.cpp" line="103"/>
+        <source>Document is printing: page %1 of %2</source>
+        <translation>Tlačí sa dokument: strana %1 z %2</translation>
+    </message>
+    <message>
+        <location filename="../src/cprintprogress.cpp" line="105"/>
+        <source>Document is preparing</source>
+        <translation>Dokument sa pripravuje</translation>
+    </message>
+    <message>
+        <location filename="../src/cprintprogress.cpp" line="111"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Zrušiť</translation>
+    </message>
+    <message>
+        <location filename="../src/cprintprogress.cpp" line="121"/>
+        <source>Printing...</source>
+        <translation>Tlačí sa ...</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/cmessage.cpp" line="129"/>
+        <source>&amp;OK</source>
+        <translation>&amp;OK</translation>
+    </message>
+</context>
+</TS>
