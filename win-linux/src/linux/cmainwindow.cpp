e3920b94e1cf41768c4ac7ca104e056e16b6ec40
maxkadushkin <maxim.kadushkin@onlyoffice.com>
2018-01-21 17:12:13 +0300

    [win-nix] fix bug 36609

--- win-linux/src/linux/cmainwindow.cpp
+++ win-linux/src/linux/cmainwindow.cpp
@@ -117,8 +117,13 @@ CMainWindow::CMainWindow(const QRect& geometry)
 
     connect(app, &SingleApplication::showUp, [=](QString args){
         QStringList * _list = Utils::getInputFiles(args.split(";"));
-        if (_list->count())
+
+        // remove app's self name from start arguments
+        if ( _list->count() ) _list->removeFirst();
+
+        if (_list->count()) {
             m_pMainPanel->doOpenLocalFiles(*_list);
+        }
 
         delete _list, _list = NULL;
 
