21183878afe466d3bae09aed3185b189e9db98c1
maxkadushkin <maxim.kadushkin@onlyoffice.com>
2018-04-26 14:26:28 +0300

    [win-nix] changed way to get certificate from user

--- win-linux/src/linux/cdialogopenssl.h
+++ win-linux/src/linux/cdialogopenssl.h
@@ -8,33 +8,7 @@
 #include <QPlainTextEdit>
 #include <QDialog>
 
-#include "../../../desktop-sdk/ChromiumBasedEditors/lib/include/applicationmanager.h"
-
-class CCertificateSelectDialogOpenSsl : public ICertificateSelectDialogOpenSsl
-{
-public:
-    QWidget*                m_pParent;
-    CAscApplicationManager* m_pManager = nullptr;
-
-    std::wstring            m_sCertPath;
-    std::wstring            m_sCertPassword;
-    std::wstring            m_sKeyPath;
-    std::wstring            m_sKeyPassword;
-
-public:
-    CCertificateSelectDialogOpenSsl(QWidget * parent = nullptr) : m_pParent(parent) {}
-    virtual ~CCertificateSelectDialogOpenSsl(){}
-
-public:
-    virtual std::wstring GetCertificatePath();
-    virtual std::wstring GetCertificatePassword();
-
-    virtual std::wstring GetKeyPath();
-    virtual std::wstring GetKeyPassword();
-
-    virtual bool ShowSelectDialog();
-    virtual int ShowCertificate(ICertificate* pCert);
-};
+#include "applicationmanager.h"
 
 class CSslDialog_Private;
 class CDialogOpenSsl : public QDialog
@@ -42,17 +16,10 @@ class CDialogOpenSsl : public QDialog
     Q_OBJECT
 
 public:
-    explicit CDialogOpenSsl(QWidget *parent, CCertificateSelectDialogOpenSsl* openssl);
+    explicit CDialogOpenSsl(QWidget *parent);
     ~CDialogOpenSsl();
 
-    virtual void resizeEvent(QResizeEvent*);
-    void CalculatePlaces();
-
-    friend class CCertificateSelectDialogOpenSsl;
-
-public:
-    bool m_bIsOK;
-
+    void getResult(NSEditorApi::CAscOpenSslData&);
 private:
     bool checkCertificate();
 
