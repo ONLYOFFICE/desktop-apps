891073500002f1dbd31d854c0eb589c83378d052
maxkadushkin <maxim.kadushkin@onlyoffice.com>
2018-12-07 19:19:26 +0300

    [win-nix] set icon for 'single' window

--- win-linux/src/linux/csinglewindow.cpp
+++ win-linux/src/linux/csinglewindow.cpp
@@ -71,6 +71,7 @@ CSingleWindow::CSingleWindow(const QRect& geometry, const QString& title, QWidge
         if ( _screen_size.height() < _window_rect.height() ) _window_rect.setHeight(_screen_size.height());
     }
 
+    setWindowIcon(Utils::appIcon());
     setMinimumSize(WINDOW_MIN_WIDTH * m_dpiRatio, WINDOW_MIN_HEIGHT * m_dpiRatio);
     setGeometry(_window_rect);
 
