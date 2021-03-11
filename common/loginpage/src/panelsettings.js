d26bb1ac53c854ad6b2c237963806274151d8a91
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2021-03-11 12:18:08 +0300

    [start page] correct some elements for dark theme

--- common/loginpage/src/panelsettings.js
+++ common/loginpage/src/panelsettings.js
@@ -156,6 +156,15 @@
             $btnApply.disable(false);
         };
 
+        function _apply_theme(name) {
+            if ( !$("body").hasClass(name) ) {
+                let _cls = document.body.className.replace(/theme-\w+/,'');
+                document.body.className = `${_cls} ${name}`;
+
+                localStorage.setItem('ui-theme', name);
+            }
+        };
+
         const _validate_user_name = name => {
             // return /^[\p{L}\p{M}\p{N}'"\.\- ]+$/u.test(name);
 
@@ -200,6 +209,8 @@
                 if ( $optsUITheme ) {
                     _new_settings.uitheme = $optsUITheme.val();
                     $optsUITheme.selectpicker('refresh');
+
+                    _apply_theme(_new_settings.uitheme);
                 }
 
                 sdk.command("settings:apply", JSON.stringify(_new_settings));
