3addc024b2631ddbe03e8dcf4f01c581c07e5832
maxkadushkin <maxim.kadushkin@onlyoffice.com>
2021-05-13 20:26:47 +0300

    [linux] debug themes for reporter window

--- win-linux/src/linux/csinglewindow.cpp
+++ win-linux/src/linux/csinglewindow.cpp
@@ -95,6 +95,8 @@ bool CSingleWindow::holdView(int id) const
 QWidget * CSingleWindow::createMainPanel(bool custom, const QString& title, QWidget * view)
 {
     QWidget * mainPanel = new QWidget;
+    mainPanel->setObjectName("mainPanel");
+    mainPanel->setProperty("uitheme", QString::fromStdWString(AscAppManager::themes().current()));
     mainPanel->setStyleSheet(AscAppManager::getWindowStylesheets(m_dpiRatio));
 
     QVBoxLayout * _layout = new QVBoxLayout(mainPanel);
