c580b27dbcbda5f3221865b5a03b4866ca661a6f
Egor Bespalov <58124384+TrueJegor@users.noreply.github.com>
2021-10-11 14:38:17 +0300

    Add startup for msi

--- win-linux/package/windows/msi_win_32.bat
+++ win-linux/package/windows/msi_win_32.bat
@@ -4,7 +4,4 @@
 :synchronization 32-bit folder
 %AdvancedInstaller% /edit "DesktopEditors.aip" /NewSync APPDIR "%~dp0..\..\..\..\build_tools\out\win_32\onlyoffice\DesktopEditors"
 
-
-
-
-start DesktopEditors.aip
+%AdvancedInstaller% /build "DesktopEditors.aip"
