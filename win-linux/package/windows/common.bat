7bce1b3b0798f43d70b781e1d511fe7a841d1197
Egor Bespalov <bespalow.e@gmail.com>
2021-10-13 18:09:24 +0300

    Update startup for msi

--- win-linux/package/windows/common.bat
+++ win-linux/package/windows/common.bat
@@ -1,3 +1,7 @@
+::if variable isn't defined
+if "%ORG%"=="" (SET ORG=onlyoffice)
+::check arch
+if %ARCH%==x86 (%AdvancedInstaller% /edit "DesktopEditors.aip" /AddOsLc -buildname DefaultBuild -arch %ARCH%)
+
 %AdvancedInstaller% /edit "DesktopEditors.aip" /NewSync APPDIR "%~dp0..\..\..\..\build_tools\out\%PLATFORM%\%ORG%\%PACKAGE%"
-%AdvancedInstaller% /edit "DesktopEditors.aip" /AddOsLc -buildname DefaultBuild -arch %ARCH%
 %AdvancedInstaller% /build DesktopEditors.aip
