a45bb88869c3a644619935a6a023eafb88a30ed5
Egor Bespalov <58124384+TrueJegor@users.noreply.github.com>
2021-10-14 15:43:00 +0300

    Update common.bat

--- win-linux/package/windows/common.bat
+++ win-linux/package/windows/common.bat
@@ -1,8 +1,8 @@
-::if variables isn't defined
+::if variable isn't defined
 if "%ORG%"=="" (SET ORG=onlyoffice)
 if "%PACKAGE%"==" (SET PACKAGE=DesktopEditors)
 ::check arch
-if "%PLATFORM%"=="win_32" (SET ARCH=x86)else if "%PLATFORM%"=="win_64"(SET ARCH=x64) else(ECHO ERROR)
+if "%PLATFORM%"=="win_32" (SET ARCH=x86)else if "%PLATFORM%"=="win_64"(SET ARCH=x64) else(EXIT)
 
 %AdvancedInstaller% /edit "DesktopEditors.aip" /AddOsLc -buildname DefaultBuild -arch %ARCH%
 %AdvancedInstaller% /edit "DesktopEditors.aip" /NewSync APPDIR "%~dp0..\..\..\..\build_tools\out\%PLATFORM%\%ORG%\%PACKAGE%"
