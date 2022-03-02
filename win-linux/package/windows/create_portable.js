fd89a8ce48e8d3460d0a7e2a4ddbcc9f8fcb92e1
Egor Bespalov <bespalow.e@gmail.com>
2022-03-02 17:16:32 +0300

    Edit code style

--- win-linux/package/windows/create_portable.js
+++ win-linux/package/windows/create_portable.js
@@ -1,3 +1,6 @@
 var generateEvb = require('generate-evb');
 
-generateEvb('DesktopEditorsPortable.evb', '%~dp0../../../../build_tools/out/%PLATFORM%/%ORG%/%PACKAGE%/DesktopEditors.exe', 'DesktopEditorsPortable.exe', '%~dp0../../../../build_tools/out/%PLATFORM%/%ORG%/%PACKAGE%');
+generateEvb('DesktopEditorsPortable.evb',
+            '%~dp0../../../../build_tools/out/%PLATFORM%/%ORG%/%PACKAGE%/DesktopEditors.exe',
+            'DesktopEditorsPortable.exe',
+            '%~dp0../../../../build_tools/out/%PLATFORM%/%ORG%/%PACKAGE%');
