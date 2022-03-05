97456e763edf95f29236daafddd74472157a17a0
Egor Bespalov <bespalow.e@gmail.com>
2022-03-05 10:16:50 +0300

    Delete optimaze.py

--- win-linux/package/windows/common.bat
+++ win-linux/package/windows/common.bat
@@ -14,5 +14,3 @@ exit
 %AdvancedInstaller% /edit "DesktopEditors.aip" /AddOsLc -buildname DefaultBuild -arch %ARCH%
 %AdvancedInstaller% /edit "DesktopEditors.aip" /NewSync APPDIR "%~dp0..\..\..\..\build_tools\out\%PLATFORM%\%ORG%\%PACKAGE%"
 %AdvancedInstaller% /build DesktopEditors.aip
-node "create_portable.js"
-%enigmavbconsole% "DesktopEditorsPortable.evb"
