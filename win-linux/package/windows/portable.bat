2d0ef6f849e224579b7dfee0d902d619a308e2e6
Egor Bespalov <bespalow.e@gmail.com>
2022-03-10 10:15:49 +0300

    Update code style

--- win-linux/package/windows/portable.bat
+++ win-linux/package/windows/portable.bat
@@ -1,14 +1,14 @@
 if not exist "node_modules/generate-evb" (
-call npm install generate-evb
+     call npm install generate-evb
 )
 set ORG=onlyoffice
 if "%PACKAGE%"=="" (set PACKAGE=DesktopEditors)
-	if "%PLATFORM%"=="win_32" (
-		set ARCH=x86
-	)else if "%PLATFORM%"=="win_64"(
-	set ARCH=x64
-	) else(
-	exit
-)
+if "%PLATFORM%"=="win_32" (
+    set ARCH=x86
+    )else if "%PLATFORM%"=="win_64"(
+        set ARCH=x64
+    ) else(
+        exit
+    )
 node create_portable.js
 %enigmavbconsole% DesktopEditorsPortable.evb
