aa9b2d1306fb503e646a54525acc6b0edbce0e1a
Egor Bespalov <bespalow.e@gmail.com>
2022-03-10 17:58:46 +0300

    Add arguments command line

--- win-linux/package/windows/create_portable.js
+++ win-linux/package/windows/create_portable.js
@@ -1,6 +1,11 @@
+const arg = process.argv;
+const PLATFORM = arg[2];
+const ORG = arg[3];
+const PACKAGE = arg[4];
+
 var generateEvb = require('generate-evb');
 
 generateEvb('DesktopEditorsPortable.evb',
-            '%~dp0../../../../build_tools/out/%PLATFORM%/%ORG%/%PACKAGE%/DesktopEditors.exe',
+            '../../../../build_tools/out/' + PLATFORM + '/' + ORG + '/' + PACKAGE + '/DesktopEditors.exe',
             'DesktopEditorsPortable.exe',
-            '%~dp0../../../../build_tools/out/%PLATFORM%/%ORG%/%PACKAGE%');
+            '../../../../build_tools/out/' + PLATFORM + '/' + ORG + '/' + PACKAGE);
