2cd7c12f355717eb3fc2305825230d6f81495ebe
Egor Bespalov <bespalow.e@gmail.com>
2022-03-10 10:30:23 +0300

    Update code style

--- win-linux/package/windows/portable.bat
+++ win-linux/package/windows/portable.bat
@@ -2,7 +2,9 @@ if not exist "node_modules/generate-evb" (
     call npm install generate-evb
 )
 set ORG=onlyoffice
-if "%PACKAGE%"=="" (set PACKAGE=DesktopEditors)
+if "%PACKAGE%"=="" ( 
+    set PACKAGE=DesktopEditors
+)
 if "%PLATFORM%"=="win_32" (
     set ARCH=x86
 ) else if "%PLATFORM%"=="win_64" (
