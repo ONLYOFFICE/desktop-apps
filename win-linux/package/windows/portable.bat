bab9760d3721b59f5d0ad388a715513781cc824b
Egor Bespalov <bespalow.e@gmail.com>
2022-03-10 10:18:10 +0300

    Update code style

--- win-linux/package/windows/portable.bat
+++ win-linux/package/windows/portable.bat
@@ -5,9 +5,9 @@ set ORG=onlyoffice
 if "%PACKAGE%"=="" (set PACKAGE=DesktopEditors)
 if "%PLATFORM%"=="win_32" (
     set ARCH=x86
-    )else if "%PLATFORM%"=="win_64"(
+    ) else if "%PLATFORM%"=="win_64" (
         set ARCH=x64
-    ) else(
+    ) else (
         exit
     )
 node create_portable.js
