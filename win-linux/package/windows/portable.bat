c7bf581b3b89b4a87e2169ecdca759367c6ab33b
Egor Bespalov <bespalow.e@gmail.com>
2022-03-15 16:17:17 +0300

    Add outpath

--- win-linux/package/windows/portable.bat
+++ win-linux/package/windows/portable.bat
@@ -8,7 +8,7 @@ if "%PACKAGE%"=="" (
 )
 
 set ORG=onlyoffice
-set OUTPATH="%~dp0..\..\..\..\build_tools\out\%PLATFORM%\%ORG%"
+set OUTPATH="%~dp0..\..\..\..\build_tools\out\%PLATFORM%\%ORG%\%PACKAGE%"
 
 if "%PLATFORM%"=="win_32" (
     set ARCH=x86
