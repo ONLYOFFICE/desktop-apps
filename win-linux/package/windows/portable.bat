e446967dcef8bb6e710b2bd04b026a6dfa48e2f2
Egor Bespalov <bespalow.e@gmail.com>
2022-03-15 16:16:39 +0300

    Add error message

--- win-linux/package/windows/portable.bat
+++ win-linux/package/windows/portable.bat
@@ -8,26 +8,23 @@ if "%PACKAGE%"=="" (
 )
 
 set ORG=onlyoffice
-set OUTPATH="%~dp0..\..\..\..\build_tools\out\%PLATFORM%\%ORG%\%PACKAGE%"
-set SYSTEM32=%SystemRoot%\System32
-set SYSWOW64=%SystemRoot%\SysWOW64
-set files[0]=msvcp140.dll
-set files[1]=vcruntime140.dll
-set files[2]=vcruntime140_1.dll
+set OUTPATH="%~dp0..\..\..\..\build_tools\out\%PLATFORM%\%ORG%"
 
 if "%PLATFORM%"=="win_32" (
     set ARCH=x86
-    for /l %%i in (0 1 1) do (
-        copy %SYTWOW64%!files[%%i]! %OUTPATH%
+    for %%I in (msvcp140.dll vcruntime140.dll) do (
+        copy %SystemRoot%\SysWOW64\%%I %OUTPATH%
         if errorlevel 1 (
+            echo dll not found
             exit 1
         )
     )
 ) else if "%PLATFORM%"=="win_64" (
     set ARCH=x64
-    for /l %%i in (0 1 2) do (
-        copy %SYTEM32%!files[%%i]! %OUTPATH%
+    for %%I in (msvcp140.dll vcruntime140.dll vcruntime140_1.dll) do (
+        copy %SystemRoot%\System32\%%I %OUTPATH%
         if errorlevel 1 (
+            echo dll not found
             exit 1
         )
     )
