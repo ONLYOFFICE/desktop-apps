83953f616ca2e3be79e806b08c9dea3abd041cbf
Egor Bespalov <bespalow.e@gmail.com>
2022-03-16 16:13:50 +0300

    Add optimaze code

--- win-linux/package/windows/portable.bat
+++ win-linux/package/windows/portable.bat
@@ -8,23 +8,23 @@ if "%PACKAGE%"=="" (
 )
 
 set ORG=onlyoffice
-set OUTPATH=%~dp0..\..\..\..\build_tools\out\%PLATFORM%\%ORG%\%PACKAGE%
-
+set OUTPATH=%~dp0..\..\..\..\build_tools\out\%PLATFORM%\%ORG%
+    set FILES=msvcp140.dll vcruntime140.dll
 if "%PLATFORM%"=="win_32" (
     set ARCH=x86
     set SOURCE=%SystemRoot%\SysWOW64
-    set files = msvcp140.dll vcruntime140.dll
 ) else if "%PLATFORM%"=="win_64" (
     set ARCH=x64
     set SOURCE=%SystemRoot%\System32
-    set files=msvcp140.dll vcruntime140.dll vcruntime140_1.dll
+    set FILES=%FILES%; vcruntime140_1.dll
 ) else (
+    echo "Architecture definition error"
     exit
 )
 
-for %%i in (%files%) do (
+for %%i in (%FILES%) do (
     copy %SOURCE%\%%i %OUTPATH%
-    ::to avoid the error, need to install vcredist 2015-2022 x64 and x86
+    rem to avoid the error, need to install vcredist 2015-2022 x64 and x86
     if errorlevel 1 (
         echo "Dll not found, need to install vcredist_%PLATFORM%"
         exit 1
