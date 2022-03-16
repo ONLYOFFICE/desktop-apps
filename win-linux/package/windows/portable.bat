ed18862dbbe194d1679eb5db4885bde2a835bca4
Egor Bespalov <bespalow.e@gmail.com>
2022-03-16 10:56:51 +0300

    Removed duplication

--- win-linux/package/windows/portable.bat
+++ win-linux/package/windows/portable.bat
@@ -12,24 +12,23 @@ set OUTPATH=%~dp0..\..\..\..\build_tools\out\%PLATFORM%\%ORG%\%PACKAGE%
 
 if "%PLATFORM%"=="win_32" (
     set ARCH=x86
-    for %%i in (msvcp140.dll vcruntime140.dll) do (
-        copy %SystemRoot%\SysWOW64\%%i %OUTPATH%
-        if errorlevel 1 (
-            echo "Dll not found"
-            exit 1
-        )
-    )
+    set SOURCE=%SystemRoot%\SysWOW64
+    set files = msvcp140.dll vcruntime140.dll
 ) else if "%PLATFORM%"=="win_64" (
     set ARCH=x64
-    for %%i in (msvcp140.dll vcruntime140.dll vcruntime140_1.dll) do (
-        copy %SystemRoot%\System32\%%i %OUTPATH%
-        if errorlevel 1 (
-            echo "Dll not found"
-            exit 1
-        )
-    )
+    set SOURCE=%SystemRoot%\System32
+    set files=msvcp140.dll vcruntime140.dll vcruntime140_1.dll
 ) else (
     exit
+)
+
+for %%i in (%files%) do (
+    copy %SOURCE%\%%i %OUTPATH%
+::to avoid the error, need to install vcredist 2015-2022 x64 and x86
+    if errorlevel 1 (
+        echo "Dll not found, need to install vcredist_%PLATFORM%"
+        exit 1
+    )
 )
 
 node create_portable.js %PLATFORM% %ORG% %PACKAGE%
