b7fbdebf1370a200a34d38160616eabc01708a85
Egor Bespalov <58124384+TrueJegor@users.noreply.github.com>
2022-03-17 13:46:35 +0300

    Add portable project (#417)
    * Add portable project
    
    * Delete excess lines
    
    * Delete excess lines
    
    * Delete excess lines
    
    * Delete excess lines
    
    * Delete excess lines
    
    * Optimization porject file
    
    * Update paths
    
    * Enable compress
    
    * Add optimaze project script
    
    * Update optimaze script
    
    * Delete main.py
    
    * Add delete empty lines
    
    * Update oprimaze script
    
    * Update oprimaze script
    
    * Update oprimaze script
    
    * Update oprimaze script
    
    * Add portable version
    
    * Delete portablepackage variable
    
    * Delete npm install
    
    * Update create_portable.js
    
    * Update code style create_portable.js
    
    * Update create_portable.js
    
    * Delete empty chars
    
    * Delete empty chars
    
    * Delete empty char
    
    * Add empty line
    
    * Add build from variables
    
    * Edit code style
    
    * Delete optimaze.py
    
    * Add build script
    
    * Update code style
    
    * Delete pause command
    
    * Update code style
    
    * Update code style
    
    * Update code style
    
    * Update code style
    
    * Update code style
    
    * Add arguments command line
    
    * Add command line arguments
    
    * Add command line arguments
    
    * Add empty line
    
    * Add literals
    
    * Add literals
    
    * Edit literals
    
    * Add copy dll
    
    * Add enigmavbconsole
    
    * Add literals
    
    * Add empty line
    
    * Add empty line
    
    * Change to vcredist
    
    * Add exception from dll
    
    * Add empty char
    
    * Add copy dll from syswow64
    
    * Add copy files
    
    * Edit index
    
    * Add error message
    
    * Add outpath
    
    * Edit code style
    
    * Edit code style
    
    * Edit code style
    
    * Edit code style
    
    * Removed duplication
    
    * Removed duplication
    
    * Add optimaze code
    
    * Add optimaze code
    
    * Add optimaze code
    
    * Add multiple copy
    
    * Edit code style
    
    Co-authored-by: papacarlo <builder@onlyoffice.com>

---
+++ win-linux/package/windows/create_portable.js
@@ -0,0 +1,11 @@
+const arg = process.argv;
+const PLATFORM = arg[2];
+const ORG = arg[3];
+const PACKAGE = arg[4];
+
+var generateEvb = require('generate-evb');
+
+generateEvb('DesktopEditorsPortable.evb',
+            '../../../../build_tools/out/' + PLATFORM + '/' + ORG + '/' + PACKAGE + '/DesktopEditors.exe',
+            'DesktopEditorsPortable.exe',
+            '../../../../build_tools/out/' + PLATFORM + '/' + ORG + '/' + PACKAGE);
---
+++ win-linux/package/windows/portable_win_64.bat
@@ -0,0 +1,2 @@
+set PLATFORM=win_64
+start portable.bat
