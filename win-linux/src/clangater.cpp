db4f2519f68b4720d7cc698e371c04e3a4341be7
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2022-07-04 18:35:46 +0300

    [win-nix] unlocked Armenian language

--- win-linux/src/clangater.cpp
+++ win-linux/src/clangater.cpp
@@ -136,7 +136,7 @@ private:
 //        ,{"hi-IN", "हिन्दी"}
 //        ,{"hr-HR", "Hrvatska"}
         ,{"hu-HU", "Magyar nyelv"}
-//        ,{"hy-AM", "Հայաստան"}
+        ,{"hy-AM", "Հայաստան"}
         ,{"id-ID", "Indonesian"}
         ,{"no-NO", "Norsk"}
         ,{"ro-RO", "Romanian"}
