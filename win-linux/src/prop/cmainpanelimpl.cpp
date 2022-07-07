d00e676c1fe86132bf9d5546643bffbb7a9a0de5
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2022-07-07 10:55:33 +0300

    [win-nix] revert method to read package info

--- win-linux/src/prop/cmainpanelimpl.cpp
+++ win-linux/src/prop/cmainpanelimpl.cpp
@@ -68,7 +68,7 @@ void CMainPanelImpl::refreshAboutVersion()
     _json_obj["link"]       = URL_SITE;
     _json_obj["changelog"]  = "https://github.com/ONLYOFFICE/DesktopEditors/blob/master/CHANGELOG.md";
 
-    QString _package = qgetenv("ASC_PACK_TYPE");
+    QString _package = QSettings(qApp->applicationDirPath() + "/converter/package.config", QSettings::IniFormat).value("package").toString();
     if ( !_package.isEmpty() )
         _json_obj["pkg"] = _package;
 
