74a9a86a0b188ca43001ee9bffb45da3a5e403a1
SimplestStudio <Oleg.Kozhukharenko@onlyoffice.com>
2022-11-23 11:53:24 +0200

    Added reset update options

--- win-linux/src/chelp.cpp
+++ win-linux/src/chelp.cpp
@@ -57,4 +57,5 @@ void CHelp::out()
     qWarning() << "    --xdg-desktop-portal=default use portals instead of gtk file chooser for current session";
     qWarning() << "    --native-file-dialog use non Qt dialog";
     qWarning() << "    --geometry=default reset window geometry";
+    qWarning() << "    --updates-reset reset all update options";
 }
