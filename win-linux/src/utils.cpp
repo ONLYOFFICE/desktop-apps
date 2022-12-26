dac10e58afafa2b16b34025239e02abccb523717
SimplestStudio <Oleg.Kozhukharenko@onlyoffice.com>
2022-12-26 18:19:08 +0200

    Toggle focus after closing modal dialog

--- win-linux/src/utils.cpp
+++ win-linux/src/utils.cpp
@@ -740,6 +740,7 @@ namespace WindowHelper {
             m_pChild->setAttribute(Qt::WA_TranslucentBackground);
             m_pChild->setGeometry(0, 0, parent->width(), parent->height());
             m_pChild->show();
+            QObject::connect(m_pChild, SIGNAL(destroyed()), parent, SLOT(setFocus()));
         }
     }
 
