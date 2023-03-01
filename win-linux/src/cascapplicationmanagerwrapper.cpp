42f4b460678b09b246b991c97ecf4c03d87c078e
maxkadushkin <maxim.kadushkin@onlyoffice.com>
2023-03-01 12:46:32 +0300

    [win] limited using Connection panel for win xp

--- win-linux/src/cascapplicationmanagerwrapper.cpp
+++ win-linux/src/cascapplicationmanagerwrapper.cpp
@@ -316,8 +316,8 @@ bool CAscApplicationManagerWrapper::processCommonEvent(NSEditorApi::CAscCefMenuE
         } else
 #ifdef Q_OS_WIN
         if ( cmd.find(L"app:onready") != std::wstring::npos ) {
-//            if ( !IsWindowsVistaOrGreater() )
-//                sendCommandTo(SEND_TO_ALL_START_PAGE, "panel:hide", "connect");
+            if ( !IsWindowsVistaOrGreater() && !InputArgs::contains(L"--xp-unlock-portals"))    // TODO: remove --xp-unlock-portals in ver 7.4, for tests only
+                sendCommandTo(SEND_TO_ALL_START_PAGE, "panel:hide", "connect");
         } else
 #endif
         if ( cmd.compare(0, 8, L"settings") == 0 ) {
