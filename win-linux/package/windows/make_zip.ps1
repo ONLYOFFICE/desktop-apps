b91f3d9bd70caab03f8f2811508d44200423b632
Semyon Bezrukov <semen.bezrukov@onlyoffice.com>
2023-08-03 17:06:09 +0300

    Fix make_zip script

--- win-linux/package/windows/make_zip.ps1
+++ win-linux/package/windows/make_zip.ps1
@@ -1,8 +1,8 @@
 param (
+    [Parameter(Mandatory)][string]$Target,
     [string]$BuildDir = "build",
     [string]$DesktopDir = "DesktopEditors",
     [string]$MultimediaDir,
-    [Parameter(Mandatory)][string]$OutFile,
     [switch]$ExcludeHelp,
     [switch]$Sign,
     [string]$CertName = "Ascensio System SIA",
@@ -11,6 +11,14 @@ param (
 
 $ErrorActionPreference = "Stop"
 Set-Location $PSScriptRoot
+
+$Suffix = switch ( $Target )
+{
+    "windows_x64"    { "x64" }
+    "windows_x86"    { "x86" }
+    "windows_x64_xp" { "x64-xp" }
+    "windows_x86_xp" { "x86-xp" }
+}
 
 # Check directory
 if ( -Not (Test-Path -Path $BuildDir) ) {
@@ -24,12 +32,12 @@ if ( $Sign ) {
         $SignFiles = Get-ChildItem *.exe, *.dll, converter\*.exe, converter\*.dll `
             | Resolve-Path -Relative
         # Sign
-        Write-Host "signtool sign /a /n $CertName /t $TimestampServer /v $SignFiles" -ForegroundColor Yellow
+        Write-Host "signtool sign /a /n $CertName /t $TimestampServer $SignFiles" -ForegroundColor Yellow
         & signtool sign /a /n $CertName /t $TimestampServer /v $SignFiles
         if ( $LastExitCode -ne 0 ) { throw }
         # Verify
-        Write-Host "signtool verify /pa /all /v $SignFiles" -ForegroundColor Yellow
-        & signtool verify /pa /all /v $SignFiles
+        Write-Host "signtool verify /pa /all $SignFiles" -ForegroundColor Yellow
+        & signtool verify /pa /all $SignFiles
         if ( $LastExitCode -ne 0 ) { throw }
 
         Set-Location $PSScriptRoot
@@ -44,8 +52,8 @@ if ( $Sign ) {
         & signtool sign /a /n $CertName /t $TimestampServer /v $SignFiles
         if ( $LastExitCode -ne 0 ) { throw }
         # Verify
-        Write-Host "signtool verify /pa /all /v $SignFiles" -ForegroundColor Yellow
-        & signtool verify /pa /all /v $SignFiles
+        Write-Host "signtool verify /pa /all $SignFiles" -ForegroundColor Yellow
+        & signtool verify /pa /all $SignFiles
         if ( $LastExitCode -ne 0 ) { throw }
 
         Set-Location $PSScriptRoot
@@ -53,11 +61,20 @@ if ( $Sign ) {
 }
 
 # Create archive
+
+$OutFile = "$Env:COMPANY_NAME-$DesktopDir-$Env:PRODUCT_VERSION.$Env:BUILD_NUMBER-$Suffix.zip"
 if ( !$ExcludeHelp ) {
-    Write-Host "7z a -y $OutFile .\$BuildDir\*" -ForegroundColor Yellow
-    & 7z a -y $OutFile .\$BuildDir\*
+    Write-Host "7z a -y $OutFile .\$BuildDir\$DesktopDir\*" -ForegroundColor Yellow
+    & 7z a -y $OutFile .\$BuildDir\$DesktopDir\*
 } else {
-    Write-Host "7z a -y $OutFile .\$BuildDir\* -xr!$DesktopDir\editors\web-apps\apps\*\main\resources\help" -ForegroundColor Yellow
-    & 7z a -y $OutFile .\$BuildDir\* -xr!$DesktopDir\editors\web-apps\apps\*\main\resources\help
+    Write-Host "7z a -y $OutFile .\$BuildDir\$DesktopDir\* -xr!editors\web-apps\apps\*\main\resources\help" -ForegroundColor Yellow
+    & 7z a -y $OutFile .\$BuildDir\$DesktopDir\* -xr!editors\web-apps\apps\*\main\resources\help
 }
 if ( $LastExitCode -ne 0 ) { throw }
+
+if ( $MultimediaDir ) {
+    $OutFile = "$Env:COMPANY_NAME-$MultimediaDir-$Env:PRODUCT_VERSION.$Env:BUILD_NUMBER-$Suffix.zip"
+    Write-Host "7z a -y $OutFile .\$BuildDir\$MultimediaDir\*" -ForegroundColor Yellow
+    & 7z a -y $OutFile .\$BuildDir\$MultimediaDir\*
+    if ( $LastExitCode -ne 0 ) { throw }
+}
