f19c99ba9e871c8191d23a58c05a5b5c989d8eb8
maxkadushkin <maxim.kadushkin@onlyoffice.com>
2023-11-21 16:47:44 +0300

    [start page] fix logout for providers with entryPage

--- common/loginpage/src/panelconnect.js
+++ common/loginpage/src/panelconnect.js
@@ -336,8 +336,13 @@
 
             let info = {domain:model.path};
             const _provider = config.portals.providers.find(model.provider);
-            if ( _provider && !!_provider.extraLogout )
-                info.extra = _provider.extraLogout;
+            if ( _provider ) {
+                if ( !!_provider.entryPage )
+                    info.domain = _provider.entryPage;
+
+                if ( !!_provider.extraLogout )
+                    info.extra = _provider.extraLogout
+            };
 
             window.sdk.execCommand('portal:logout', JSON.stringify(info));
         };
