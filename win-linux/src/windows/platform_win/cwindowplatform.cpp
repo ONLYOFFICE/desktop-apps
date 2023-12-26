028e78741854a3453c8d3f6b899926e96b6b1013
SimplestStudio <Oleg.Kozhukharenko@onlyoffice.com>
2023-12-26 11:41:05 +0200

    [win] cwindowplatform: comment out unused code

--- win-linux/src/windows/platform_win/cwindowplatform.cpp
+++ win-linux/src/windows/platform_win/cwindowplatform.cpp
@@ -40,7 +40,7 @@
 #include <QScreen>
 #include <shellapi.h>
 
-#define UM_SNAPPING 0x02
+//#define UM_SNAPPING 0x02
 #define DCX_USESTYLE 0x00010000
 #define NC_AREA_WIDTH 3
 #define SKIP_EVENTS_QUEUE(callback) QTimer::singleShot(0, this, callback)
@@ -170,8 +170,8 @@ CWindowPlatform::CWindowPlatform(const QRect &rect) :
     m_resAreaWidth(MAIN_WINDOW_BORDER_WIDTH),
     m_borderless(true),
     m_closed(false),
-    m_isResizeable(true),
-    m_allowMaximize(true)
+    m_isResizeable(true)
+//    m_allowMaximize(true)
 {
     m_isThemeActive = isThemeActive();
     m_isTaskbarAutoHideOn = isTaskbarAutoHideOn();
@@ -204,18 +204,28 @@ CWindowPlatform::~CWindowPlatform()
 
 /** Public **/
 
-void CWindowPlatform::toggleBorderless(bool showmax)
-{
-    if (isVisible()) {
-        m_borderless = !m_borderless;
-        show(showmax);
-    }
-}
+//void CWindowPlatform::toggleBorderless(bool showmax)
+//{
+//    if (isVisible()) {
+//        m_borderless = !m_borderless;
+//        show(showmax);
+//    }
+//}
 
-void CWindowPlatform::toggleResizeable()
-{
-    m_isResizeable = !m_isResizeable;
-}
+//void CWindowPlatform::toggleResizeable()
+//{
+//    m_isResizeable = !m_isResizeable;
+//    auto flags = windowFlags();
+//    if (m_isResizeable) {
+//        setMinimumSize(0,0);
+//        setMaximumSize(QWIDGETSIZE_MAX,QWIDGETSIZE_MAX);
+//        flags |= Qt::WindowMaximizeButtonHint;
+//    } else {
+//        setFixedSize(size());
+//        flags &= ~Qt::WindowMaximizeButtonHint;
+//    }
+//    setWindowFlags(flags);
+//}
 
 void CWindowPlatform::bringToTop()
 {
@@ -300,7 +310,7 @@ bool CWindowPlatform::nativeEvent(const QByteArray &eventType, void *message, lo
     MSG* msg = reinterpret_cast<MSG*>(message);
 #endif
 
-    static uchar movParam = 0;
+//    static uchar movParam = 0;
     switch (msg->message)
     {
     case WM_ACTIVATE: {
@@ -333,14 +343,14 @@ bool CWindowPlatform::nativeEvent(const QByteArray &eventType, void *message, lo
         break;
     }
 
-    case WM_SYSKEYDOWN: {
-        if (msg->wParam == VK_SPACE) {
-            RECT winrect;
-            GetWindowRect(msg->hwnd, &winrect);
-            TrackPopupMenu(GetSystemMenu(msg->hwnd, false ), TPM_TOPALIGN | TPM_LEFTALIGN, winrect.left + 5, winrect.top + 5, 0, msg->hwnd, NULL);
-        }
-        break;
-    }
+//    case WM_SYSKEYDOWN: {
+//        if (msg->wParam == VK_SPACE) {
+//            RECT winrect;
+//            GetWindowRect(msg->hwnd, &winrect);
+//            TrackPopupMenu(GetSystemMenu(msg->hwnd, false ), TPM_TOPALIGN | TPM_LEFTALIGN, winrect.left + 5, winrect.top + 5, 0, msg->hwnd, NULL);
+//        }
+//        break;
+//    }
 
     case WM_NCCALCSIZE: {
         if (!m_borderless || !msg->wParam)
@@ -485,20 +495,20 @@ bool CWindowPlatform::nativeEvent(const QByteArray &eventType, void *message, lo
         break;
     }
 
-    case WM_EXITSIZEMOVE:
-        if (m_allowMaximize)
-            QApplication::postEvent(this, new QEvent(QEvent::User));
-        break;
+//    case WM_EXITSIZEMOVE:
+//        if (m_allowMaximize)
+//            QApplication::postEvent(this, new QEvent(QEvent::User));
+//        break;
 
-    case WM_MOVE:
-        if (movParam != 0)
-            movParam = 0;
-        break;
+//    case WM_MOVE:
+//        if (movParam != 0)
+//            movParam = 0;
+//        break;
 
-    case WM_MOVING:
-        if (m_allowMaximize && ++movParam == UM_SNAPPING)
-            m_allowMaximize = false;
-        break;
+//    case WM_MOVING:
+//        if (m_allowMaximize && ++movParam == UM_SNAPPING)
+//            m_allowMaximize = false;
+//        break;
 
     case WM_SIZING:
         if (m_borderless)
