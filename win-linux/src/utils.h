b3476b97b87b1e590683f849d787212de3bd9fc9
SimplestStudio <Oleg.Kozhukharenko@onlyoffice.com>
2024-01-29 18:37:32 +0200

    [win] fix bug 65991

--- win-linux/src/utils.h
+++ win-linux/src/utils.h
@@ -158,6 +158,7 @@ namespace WindowHelper {
     auto correctModalOrder(HWND windowhandle, HWND modalhandle) -> void;
     auto adjustWindowRect(HWND, double, LPRECT) -> void;
     auto bringToTop(HWND) -> void;
+    auto toggleLayoutDirection(HWND hwnd) -> void;
 #endif
 
     auto correctWindowMinimumSize(const QRect&, const QSize&) -> QSize;
