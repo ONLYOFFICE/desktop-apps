18ec55ca35825b47a7cac14555d50b45fccc18f0
maxkadushkin <maxim.kadushkin@onlyoffice.com>
2024-02-07 13:37:57 +0300

    [win-nix] fix bug 66400

--- win-linux/src/cascapplicationmanagerwrapper.cpp
+++ win-linux/src/cascapplicationmanagerwrapper.cpp
@@ -1232,7 +1232,7 @@ void CAscApplicationManagerWrapper::initializeApp()
     if ( !local_themes_array.isEmpty() )
         EditorJSVariables::setVariable("localthemes", local_themes_array);
 
-#if not defined(__OS_WIN_XP)
+#if !defined(__OS_WIN_XP)
     const bool _is_rtl = reg_user.contains("forcedRtl") ? reg_user.value("forcedRtl", false).toBool() :
                        CLangater::isRtlLanguage(CLangater::getCurrentLangCode());
 #else
