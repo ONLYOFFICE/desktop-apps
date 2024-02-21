48dc3a48da0cc5c471a6c0759c29095e816d2ff1
Mikhail Lobotskiy <mikhail.lobotskiy@onlyoffice.com>
2024-02-21 13:34:02 +0400

    Change tabs to spaces in controller code

--- macos/ONLYOFFICE/Code/Controllers/Common/ASCCommonViewController.mm
+++ macos/ONLYOFFICE/Code/Controllers/Common/ASCCommonViewController.mm
@@ -1048,15 +1048,15 @@
 
         CAscApplicationManager * appManager = [NSAscApplicationWorker getAppManager];
 
-		// using synchronization to be sure that flag `ASCPrinterContext::isCurrentlyPrinting` is correctly handled
-		static dispatch_queue_t printQueue = dispatch_queue_create(NULL, NULL);
-		dispatch_sync(printQueue, ^{
-			if (appManager && !ASCPrinterContext::isCurrentlyPrinting) {
-				m_pContext = new ASCPrinterContext(appManager);
-				//            m_pContext->BeginPaint([viewId intValue], [pagesCount intValue], self, @selector(printOperationDidRun:success:contextInfo:));
-				m_pContext->BeginPaint(notification.userInfo, self, @selector(printOperationDidRun:success:contextInfo:));
-			}
-		});
+        // using synchronization to be sure that flag `ASCPrinterContext::isCurrentlyPrinting` is correctly handled
+        static dispatch_queue_t printQueue = dispatch_queue_create(NULL, NULL);
+        dispatch_sync(printQueue, ^{
+            if (appManager && !ASCPrinterContext::isCurrentlyPrinting) {
+                m_pContext = new ASCPrinterContext(appManager);
+                //            m_pContext->BeginPaint([viewId intValue], [pagesCount intValue], self, @selector(printOperationDidRun:success:contextInfo:));
+                m_pContext->BeginPaint(notification.userInfo, self, @selector(printOperationDidRun:success:contextInfo:));
+            }
+        });
     }
 }
 
