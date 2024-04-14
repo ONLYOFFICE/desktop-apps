6eda94200429c430c90de251631c5d2ac0433f4b
maxkadushkin <maxim.kadushkin@onlyoffice.com>
2024-04-14 23:03:15 +0300

    [win-nix] remove unused code

--- win-linux/src/cascapplicationmanagerwrapper.cpp
+++ win-linux/src/cascapplicationmanagerwrapper.cpp
@@ -971,10 +971,6 @@ void CAscApplicationManagerWrapper::handleInputCmd(const std::vector<wstring>& v
                 continue;
         }
 
-        // TODO: remove for ver 7.2. skip single window for --review flag without --forse-use-window
-        // if ( open_in_new_window )
-        //    open_in_new_window = std::find(vargs.begin(), vargs.end(), L"--force-use-tab") == std::end(vargs);
-        //
 
         CTabPanel * panel = CEditorTools::createEditorPanel(open_opts, CWindowBase::expectedContentSize(_start_rect, open_in_new_window));
         if ( panel ) {
