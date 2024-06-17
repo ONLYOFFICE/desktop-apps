8f2a93cf91d2ba2236b5599e6580c95d552c50dc
Semyon Bezrukov <semen.bezrukov@onlyoffice.com>
2024-06-17 19:14:29 +0300

    Refactoring msi package (#1255)

--- win-linux/package/windows/make_advinst.ps1
+++ win-linux/package/windows/make_advinst.ps1
@@ -22,6 +22,17 @@ $MsiBuild = switch ($Arch) {
     "x64" { "MsiBuild64" }
     "x86" { "MsiBuild32" }
 }
+$LanguageCodes = @(1029, 1031, 1033, 1036, 1041, 1046, 1049, 1060, 2070, 3082)
+$AssociationList = @(
+    "doc", "dot", "docm", "dotm", "docx", "dotx",
+    "xls", <#"xlt",#> "xlsm", "xltm", "xlsb", "xlsx", "xltx",
+    "ppt", "pot", "pps", "pptm", "potm", "ppsm", "pptx", "potx", "ppsx",
+    "odt", "ott", "ods", "ots", "odp", "otp",
+    "djvu", "fb2", "pdf", "rtf", "xps", "oxps",
+    "epub", "html", "xml",
+    "csv", "txt",
+    "docxf"
+)
 $MD5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
 $ASCII = New-Object -TypeName System.Text.ASCIIEncoding
 [guid]$GUID = $MD5.ComputeHash($ASCII.GetBytes(`
@@ -65,71 +76,21 @@ Write-Output "package=msi" `
 
 Write-Host "`n[ Create Advanced Installer config ]"
 
-$AdvInstConfig = @(
-    "DelFolder CUSTOM_PATH", `
-    "SetProductCode -langid 1029 -guid $ProductCode", `
-    "SetProductCode -langid 1031 -guid $ProductCode", `
-    "SetProductCode -langid 1033 -guid $ProductCode", `
-    "SetProductCode -langid 1036 -guid $ProductCode", `
-    "SetProductCode -langid 1041 -guid $ProductCode", `
-    "SetProductCode -langid 1046 -guid $ProductCode", `
-    "SetProductCode -langid 1049 -guid $ProductCode", `
-    "SetProductCode -langid 1060 -guid $ProductCode", `
-    "SetProductCode -langid 2070 -guid $ProductCode", `
-    "SetProductCode -langid 3082 -guid $ProductCode", `
-    "SetProperty FORMS=1"
-)
+$AdvInstConfig = @()
+if (-not $Sign) {
+    $AdvInstConfig += "ResetSig"
+}
 if ($Arch -eq "x86") {
     $AdvInstConfig += `
         "SetComponentAttribute -feature_name MainFeature -unset -64bit_component", `
         "SetComponentAttribute -feature_name Files -unset -64bit_component", `
         "SetComponentAttribute -feature_name Registry -unset -64bit_component", `
         "SetComponentAttribute -feature_name UpdateService -unset -64bit_component", `
-        "SetComponentAttribute -feature_name RegFileTypeAssociations -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_DOC -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_DOT -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_DOCM -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_DOTM -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_DOCX -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_DOTX -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_XLS -unset -64bit_component", `
-        # "SetComponentAttribute -feature_name FA_XLT -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_XLSM -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_XLTM -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_XLSB -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_XLSX -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_XLTX -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_PPT -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_POT -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_PPS -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_PPTM -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_POTM -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_PPSM -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_PPTX -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_POTX -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_PPSX -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_ODT -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_OTT -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_ODS -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_OTS -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_ODP -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_OTP -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_DJVU -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_FB2 -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_PDF -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_RTF -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_XPS -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_OXPS -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_EPUB -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_HTML -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_XML -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_CSV -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_TXT -unset -64bit_component", `
-        "SetComponentAttribute -feature_name FA_DOCXF -unset -64bit_component"
+        "SetComponentAttribute -feature_name RegFileTypeAssociations -unset -64bit_component"
+    $AssociationList | % {$AdvInstConfig += `
+            "SetComponentAttribute -feature_name FA_$($_.ToUpper()) -unset -64bit_component"}
 }
-if (-not $Sign) {
-    $AdvInstConfig += "ResetSig"
-}
+$LanguageCodes | % {$AdvInstConfig += "SetProductCode -langid $_ -guid $ProductCode"}
 $AdvInstConfig += `
     "SetProperty Version=$VersionShort", `
     "SetVersion $Version -noprodcode", `
@@ -157,9 +118,9 @@ if ($LastExitCode -ne 0) { throw }
 Write-Host "`n[ Fix Summary Information Properties ]"
 
 $Template = ";1033,1049,1029,1031,3082,1036,2070,1046,1060,1041,0"
-switch ($Arch) {
-    "x64" { $Template = "x64" + $Template }
-    "x86" { $Template = "Intel" + $Template }
+$Template = switch ($Arch) {
+    "x64" { "x64" + $Template }
+    "x86" { "Intel" + $Template }
 }
 
 Write-Host "MsiInfo $MsiFile /p $Template"
