745986c58b5a32fe39249c1b21307cf469ea65e9
maxkadushkin <maxim.kadushkin@onlyoffice.com>
2024-06-28 19:22:11 +0300

    [win-nix] added new theme

--- win-linux/src/cthemes.cpp
+++ win-linux/src/cthemes.cpp
@@ -178,6 +178,7 @@ public:
             {"theme-classic-light", ":/themes/theme-classic-light.json"},
             {"theme-dark", ":/themes/theme-dark.json"},
             {"theme-contrast-dark", ":/themes/theme-contrast-dark.json"},
+            {"theme-gray", ":/themes/theme-light.json"},
         };
 
         GET_REGISTRY_USER(_reg_user);
