551993f83d75ce42d6255d011dde1834238ce980
maxkadushkin <maxim.kadushkin@onlyoffice.com>
2024-10-10 18:39:10 +0300

    [start] changed localization for "Gray" theme

--- common/loginpage/src/locale.js
+++ common/loginpage/src/locale.js
@@ -121,6 +121,7 @@ l10n.en = {
     settOptThemeLight: 'Light',
     settOptThemeClassicLight: 'Classic Light',
     settOptThemeDark: 'Dark',
+    settOptThemeGray: 'Gray',
     settOptLaunchMode: 'Open file',
     settOptLaunchInTab: 'In its own tab',
     settOptLaunchInWindow: 'In its own window',
