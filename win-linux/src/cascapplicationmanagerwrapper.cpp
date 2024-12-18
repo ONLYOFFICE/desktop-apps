2916a769cb4ee92e206ad3a5c6c9c7d09dd0c32f
SimplestStudio <Oleg.Kozhukharenko@onlyoffice.com>
2024-12-18 11:51:22 +0200

    [win-linux] comment out unused code

--- win-linux/src/cascapplicationmanagerwrapper.cpp
+++ win-linux/src/cascapplicationmanagerwrapper.cpp
@@ -926,7 +926,7 @@ void CAscApplicationManagerWrapper::handleInputCmd(const std::vector<wstring>& v
                 open_opts.srctype = etNewFile;
                 open_opts.format = arg.rfind(L"cell") != wstring::npos ? AVS_OFFICESTUDIO_FILE_SPREADSHEET_XLSX :
                                     arg.rfind(L"slide") != wstring::npos ? AVS_OFFICESTUDIO_FILE_PRESENTATION_PPTX :
-                                    arg.rfind(L"draw") != wstring::npos ? AVS_OFFICESTUDIO_FILE_DRAW_VSDX :
+                                    // arg.rfind(L"draw") != wstring::npos ? AVS_OFFICESTUDIO_FILE_DRAW_VSDX :
                                     arg.rfind(L"form") != wstring::npos ? AVS_OFFICESTUDIO_FILE_DOCUMENT_DOCXF :
                             /*if ( line.rfind(L"word") != wstring::npos )*/ AVS_OFFICESTUDIO_FILE_DOCUMENT_DOCX;
 
@@ -2254,7 +2254,7 @@ QString CAscApplicationManagerWrapper::newFileName(const std::wstring& format)
     int _f = format == L"word" ? AVS_OFFICESTUDIO_FILE_DOCUMENT_DOCX :
                  format == L"cell" ? AVS_OFFICESTUDIO_FILE_SPREADSHEET_XLSX :
                  format == L"form" ? AVS_OFFICESTUDIO_FILE_DOCUMENT_DOCXF :
-                 format == L"draw" ? AVS_OFFICESTUDIO_FILE_DRAW_VSDX :
+                 // format == L"draw" ? AVS_OFFICESTUDIO_FILE_DRAW_VSDX :
                  format == L"slide" ? AVS_OFFICESTUDIO_FILE_PRESENTATION_PPTX : AVS_OFFICESTUDIO_FILE_UNKNOWN;
 
     return newFileName(_f);
