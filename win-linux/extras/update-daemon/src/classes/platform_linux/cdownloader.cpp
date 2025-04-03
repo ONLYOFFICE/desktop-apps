4c117cd6c4b4378ce494da5fd6e944ae9ed831a5
SimplestStudio <Oleg.Kozhukharenko@onlyoffice.com>
2025-04-03 13:58:26 +0300

    [win-linux] updatesvc: debug cdownloader

--- win-linux/extras/update-daemon/src/classes/platform_linux/cdownloader.cpp
+++ win-linux/extras/update-daemon/src/classes/platform_linux/cdownloader.cpp
@@ -93,14 +93,18 @@ void CDownloader::queryContentLenght(const string &url)
 
     pimpl->m_lock = true;
     pimpl->m_future = std::async(std::launch::async, [=]() {
-        double fileSize = 0;
+        curl_off_t fileSize = 0;
         CURLcode res = CURLE_FAILED_INIT;
         if (CURL *curl = curl_easy_init()) {
             curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
             curl_easy_setopt(curl, CURLOPT_NOBODY, 1L);
+            curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+            curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
             res = curl_easy_perform(curl);
-            if (res == CURLE_OK)
-                curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD, &fileSize);
+            if (res == CURLE_OK) {
+                curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD_T, &fileSize);
+                if (fileSize < 0) fileSize = 0;
+            }
             curl_easy_cleanup(curl);
         }
         int error = (res == CURLE_OK) ? 0 :
@@ -137,6 +141,7 @@ void CDownloader::start()
             if (CURL *curl = curl_easy_init()) {
                 pimpl->m_prev_percent = -1;
                 curl_easy_setopt(curl, CURLOPT_URL, pimpl->m_url.c_str());
+                curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
                 curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
                 curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
                 curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0);
