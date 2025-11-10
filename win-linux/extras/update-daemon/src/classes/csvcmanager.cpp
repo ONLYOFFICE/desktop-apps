4052e420c52e776ecb25bb89d11a04b414ef1843
SimplestStudio <Oleg.Kozhukharenko@onlyoffice.com>
2025-11-10 17:28:21 +0200

    [win] updmanager: adaptation for arm64

--- win-linux/extras/update-daemon/src/classes/csvcmanager.cpp
+++ win-linux/extras/update-daemon/src/classes/csvcmanager.cpp
@@ -95,9 +95,11 @@ using std::vector;
 auto currentArch()->tstring
 {
 #ifdef _WIN32
-# ifdef _WIN64
+# if defined(_M_ARM64)
+    return L"_arm64";
+# elif defined(_M_X64)
     return L"_x64";
-# else
+# elif defined(_M_IX86)
     return L"_x86";
 # endif
 #else
@@ -513,9 +515,11 @@ void CSvcManager::onCompleteSlot(const int error, const tstring &filePath)
                 tstring curr_svc_version = _T(VER_FILEVERSION_STR);
                 JsonObject package = root.value(_T("package")).toObject();
 #ifdef _WIN32
-# ifdef _WIN64
+# if defined(_M_ARM64)
+                JsonObject win = package.value(_T("win_arm64")).toObject();
+# elif defined(_M_X64)
                 JsonObject win = package.value(_T("win_64")).toObject();
-# else
+# elif defined(_M_IX86)
                 JsonObject win = package.value(_T("win_32")).toObject();
 # endif
 #else
